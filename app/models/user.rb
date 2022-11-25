34cc0ed6fdf65787d76bcf754ee55d93447d6257
Raul Ospina <raminka13@hotmail.com>
2022-11-25 12:21:56 -0500

    Change Uniqueness for first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true, uniqueness: true,
+  validates :first_name, presence: true,
                          length: { maximum: 18, too_long: '%<count>s characters is the maximum allowed' }
-  validates :last_name, presence: true, uniqueness: true,
-                        length: { maximum: 18, too_long: '%<count>s characters is the maximum allowed' }
+  validates :last_name, presence: true, length: { maximum: 18, too_long: '%<count>s characters is the maximum allowed' }
   validates :username, presence: true, uniqueness: true,
                        length: { maximum: 18, too_long: '%<count>s characters is the maximum allowed' }
 end
