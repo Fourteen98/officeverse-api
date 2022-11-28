2c3a0ff383652c800a3db9b047120b2542682735
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-28 15:18:59 +0000

    add office controller actions

---
+++ test/controllers/offices_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class OfficesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get offices_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get offices_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get offices_new_url
+    assert_response :success
+  end
+end
