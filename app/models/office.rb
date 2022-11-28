15d34e6f3b1058c9d967ce7e86f353e2429c8f94
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-28 15:12:04 +0000

    add office table validations

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,2 +1,9 @@
 class Office < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :area, presence: true
+  validates :occupancy, presence: true, numericality: { only_integer: true }
+  validates :images, presence: true
+  validates :basic_price, presence: true, numericality: true
+  validates :address, presence: true
 end
