069546de0d1a5103061e2892bd974d43bcbde42b
Diego Yon <diegoyon@outlook.com>
2022-11-29 12:05:05 -0600

    add users office relationship

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,4 +1,6 @@
 class Office < ApplicationRecord
+  belongs_to :user
+
   has_many :reservations
   has_many :users, through: :reservations
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :offices
 
   has_many :reservations
   has_many :offices, through: :reservations
