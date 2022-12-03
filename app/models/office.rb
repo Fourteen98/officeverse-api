9c9badb217c7eaecdad4320e0ebbc8148bcb5640
Raul Ospina <raminka13@hotmail.com>
2022-12-03 11:32:48 -0500

    Improve offices controller

--- app/models/office.rb
+++ app/models/office.rb
@@ -8,7 +8,6 @@ class Office < ApplicationRecord
   validates :description, presence: true
   validates :area, presence: true
   validates :occupancy, presence: true, numericality: { only_integer: true }
-  validates :images, presence: true
   validates :basic_price, presence: true, numericality: true
   validates :address, presence: true
 end
