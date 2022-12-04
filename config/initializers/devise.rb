cfb9eaf11e9a9d2b47090770a7627d49f4a5cb21
jaferidrees <jaferll@gmail.com>
2022-12-04 20:41:11 +0100

    Add secrets.yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,17 +10,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
-  # Devise-jwt setup
-  config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
-    jwt.dispatch_requests = [
-      ['POST', %r{^/login$}]
-    ]
-    jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$}]
-    ]
-    jwt.expiration_time = 30.minutes.to_i
-end
+ 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -320,4 +310,15 @@ end
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+   # Devise-jwt setup
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.secrets.fetch(:secret_key_base)
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 90.minutes.to_i
+  end
 end
