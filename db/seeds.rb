4939064a6df9330fd4083ae7b4fc63677af9540f
Diego Yon <diegoyon@outlook.com>
2022-12-05 15:15:34 -0600

    add styles to reservation form

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,19 @@
 require 'faker'
 
 9.times do
-  User.create(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name, username: Faker::Name.middle_name)
-  Service.create(name: Faker::Lorem.word, description: Faker::Lorem.sentence, price: Faker::Number.decimal(l_digits: 2))
-  Peripheral.create(name: Faker::Appliance.equipment, description: Faker::Lorem.sentence, price: Faker::Number.decimal(l_digits: 2))
+  User.create!(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name, username: Faker::Internet.username, email: Faker::Internet.email, password: 'password')
+  Service.create!(name: Faker::Lorem.word, description: Faker::Lorem.sentence, price: Faker::Number.decimal(l_digits: 2))
+  Peripheral.create!(name: Faker::Appliance.equipment, description: Faker::Lorem.sentence, price: Faker::Number.decimal(l_digits: 2))
 end
 
 9.times do |office|
-  Office.create(title: Faker::Lorem.word, description: Faker::Lorem.sentence,
+  Office.create!(title: Faker::Lorem.word, description: Faker::Lorem.sentence,
                 area: rand(1...72), occupancy: rand(1...9),
                 images: [Faker::LoremFlickr.image(size: "400x300", search_terms: ['office']), Faker::LoremFlickr.image(size: "400x300", search_terms: ['offices'])], basic_price: Faker::Number.decimal(l_digits: 2), address: Faker::Address.full_address, user_id: office+1)
 end
 
 9.times do |reservation|
-  Reservation.create(start_date: Faker::Date.between(from: 4.days.ago, to: 2.days.ago), end_date: Faker::Date.between(from: 1.days.ago, to: Date.today), peripherals: [], services: [], user_id: reservation+1, office_id: reservation+1)
+  Reservation.create!(start_date: Faker::Date.between(from: 4.days.ago, to: 2.days.ago), end_date: Faker::Date.between(from: 1.days.ago, to: Date.today), peripherals: [], services: [], user_id: reservation+1, office_id: reservation+1)
 end
 
 p 'Created 9 users'
